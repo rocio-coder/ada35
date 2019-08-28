d1f3430bf86126768616f98878bd7c589e6009c5
rocio-corder <suarez.rocio96@gmail.com>
2019-08-28 20:44:16 -0300

    Se agrego una linea nueva

---
+++ src/Ada.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Ada {
+    public static void main(String[] args) {
+        System.out.println("ingrese el area");
+        System.out.println("ingrese el area");
+        System.out.println("ingrese el area");
+        System.out.println("ingrese el area");
+        System.out.println("ingrese el area");
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("ingres numero");
+
+    }
+}
